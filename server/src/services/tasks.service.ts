1cb3b699b4b81dd289760dd2918959c6ee8a2fe5
Rodrigodeveloperjr <rodrigojsdeveloper@gmail.com>
2023-02-22 13:55:23 -0300

    Refactor: creating user in task creation and visualization and array task in user creation and visualization

--- server/src/services/tasks.service.ts
+++ server/src/services/tasks.service.ts
@@ -2,21 +2,35 @@ import { taskRepository } from "../repositories/task.repository";
 import { NotFoundError } from "../errors/notFound.error";
 import { ITask } from "../interfaces/task.interface";
 import { Task } from "../entities/task.entity";
+import { userRepository } from "../repositories/user.repository";
 
 class TasksServices {
-  public async create(task: ITask): Promise<Task> {
-    const newTask = taskRepository.create({ ...task });
+  public async create(task: ITask, email: string): Promise<Task> {
+    const user = await userRepository.findOneBy({ email });
+
+    if (!user) {
+      throw new NotFoundError("User");
+    }
+
+    Reflect.deleteProperty(user, "tasks");
+
+    const newTask = taskRepository.create({ ...task, user });
     await taskRepository.save(newTask);
 
     return newTask;
   }
 
   public async specific(task_id: string): Promise<Task> {
-    const task = await taskRepository.findOneBy({ id: task_id });
+    const task = await taskRepository.findOne({
+      where: { id: task_id },
+      relations: ["user"],
+    });
 
     if (!task) {
       throw new NotFoundError("Task");
     }
+
+    Reflect.deleteProperty(task.user, "tasks");
 
     return task;
   }
